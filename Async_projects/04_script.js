8cff80ae0f41560b394ecbd227870fdf46415af8
Tushar Viradiya <tusharviradiya2@gmail.com>
2024-02-08 15:26:43 +0530

    key press project is completed

---
+++ Async_projects/04_script.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  insert.innerHTML = `
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keydown</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    `;
+});
